768ad53d45f13bcd9750d0792fa19cf233ecae8d
somasekimoto <0421.soma@gmail.com>
2023-09-10 17:35:10 +0900

    add amplify rest api demo

---
+++ amplify_drone/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "api8f1bec4d": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "todo": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
